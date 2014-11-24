35c71bb6696bcfdb68d9eabe5995a33a24803c5d
Michael Schober <schober4@uw.edu>
2014-11-23 23:45:33 -0800

    Blink -->  Fade

--- BlinkTest.ino
+++ BlinkTest.ino
@@ -1,24 +1,35 @@
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
+ Fade
  
-  This example code is in the public domain.
+ This example shows how to fade an LED on pin 9
+ using the analogWrite() function.
+ 
+ This example code is in the public domain.
  */
- 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-int led = 9;
+
+int led = 9;           // the pin that the LED is attached to
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
-}
+void setup()  { 
+  // declare pin 9 to be an output:
+  pinMode(led, OUTPUT);
+} 
 
 // the loop routine runs over and over again forever:
-void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
+void loop()  { 
+  // set the brightness of pin 9:
+  analogWrite(led, brightness);    
+
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+
+  // reverse the direction of the fading at the ends of the fade: 
+  if (brightness == 0 || brightness == 255) {
+    fadeAmount = -fadeAmount ; 
+  }     
+  // wait for 30 milliseconds to see the dimming effect    
+  delay(30);                            
 }
+
